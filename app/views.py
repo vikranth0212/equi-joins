1cf00bc5e298b70ddebd65d8887657a18cd3730c
vikranth0212 <150246583+vikranth0212@users.noreply.github.com>
2024-01-30 19:04:51 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -4,8 +4,40 @@ from app.models import *
 
 def equijoin(request):
     EMO=Emp.objects.select_related('deptno').all()
+    #select_related for 1 to 1,t 10 many
+    #it will join then it will retrive
+    #prefetch_related for 1 to 1,1 to many,many to 1,many to many
+    #it will fetch/retrive then join
+    #deptno is common column
+    EMO=Emp.objects.select_related('deptno').filter(hiredate__year=2024)
+    EMO=Emp.objects.select_related('deptno').filter(sal__lt=4000)
+    EMO=Emp.objects.select_related('deptno').filter(sal__gte=4000)
+    EMO=Emp.objects.select_related('deptno').filter(sal__gt=4000)
+    EMO=Emp.objects.select_related('deptno').filter(sal__gte=4000,sal__lt=5000)
+    EMO=Emp.objects.select_related('deptno').filter(mgr__isnull=True)
+    #if mgr is not there we need to give isnull=True
+    EMO=Emp.objects.select_related('deptno').filter(mgr__isnull=False)
+    #false will get who and all have mgr
+    EMO=Emp.objects.select_related('deptno').filter(ename='vicky')
+    #EMO is emp table object,to get data from same table we r using columnname=value
+    EMO=Emp.objects.select_related('deptno').filter(deptno__deptno=10)
+    #for another table common column__columnname=value
+    
+    EMO=Emp.objects.select_related('deptno').filter(sal__gt=4000,sal__lt=5000)
+    #for concatenation we are using comma like plus sym,pipe symbol
+    EMO=Emp.objects.select_related('deptno').all()
+    EMO=Emp.objects.select_related('deptno').filter(deptno__dloc='singapore')
+    #print(EMO)
+    #EMO=Emp.objects.select_related('deptno').filter(deptno__dname='ui/ux')
+    
+
+   
+    
     d={'EMO' : EMO}
     return render (request,'equijoin.html',d)
 
-    #select_related for 1 to 1,t 10 many
-    #prefetch_related for 1 to 1,1 to many,many to 1,many to many
+
+    #for printing data
+     #same table objectname.columnname,other table objname.commoncol.colname
+    #for writing a condition
+    # sametable colname=value,other table commonname__colname=value 
